28a0efb4e06ded60978bcd7ae3599bc947c6d2b6
GordonFern <gordon.fern@btinternet.com>
2014-04-01 17:18:53 +0100

    Made interactive

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,6 @@
 #Default is "World"
 #Author: Gordon Fern (gordon.fern@btinternet.com)
-name=ARGV.first || "World"
-puts "Hello, #{name}!"
+puts "What's your name"
+my_name = gets.strip
+
+puts "Hello, #{my_name}!"
